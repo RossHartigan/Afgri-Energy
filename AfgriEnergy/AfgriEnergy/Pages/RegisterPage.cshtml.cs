da5fca6af10e9bee40a16b4cfecacd9bfbe405f8
Ross Hartigan <rosshartigan@gmail.com>
2024-05-31 10:18:32 +0200

    Code 1
    updated code

---
+++ AfgriEnergy/AfgriEnergy/Pages/RegisterPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AfgriEnergy.Pages
+{
+    public class RegisterPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
